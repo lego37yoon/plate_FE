26c9889059c083eb7378c18288954b31c3f36afa
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-05-25 23:00:55 +0900

    fix: use public address for uploaded object

--- src/lib/s3/client.ts
+++ src/lib/s3/client.ts
@@ -1,4 +1,4 @@
-import { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, S3_BUCKET, S3_ENDPOINT, S3_REGION } from "$env/static/private";
+import { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, S3_ALIAS, S3_BUCKET, S3_ENDPOINT, S3_REGION } from "$env/static/private";
 import { S3, type PutObjectRequest } from "@aws-sdk/client-s3";
 import { Readable } from "node:stream";
 import { ReadableStream } from "node:stream/web";
@@ -7,7 +7,7 @@ export default function ConnectStorageProvider(path: string | null = ""): S3 {
   return new S3({
     apiVersion: "latest",
     region: S3_REGION,
-    endpoint: `${S3_ENDPOINT}${path}`,
+    endpoint: `${S3_ENDPOINT}/${path}`,
     credentials: {
       accessKeyId: AWS_ACCESS_KEY,
       secretAccessKey: AWS_SECRET_ACCESS_KEY
@@ -30,7 +30,7 @@ export async function UploadObject(file: File, name: string, path: string | null
 
     try {
       await s3.putObject(params);
-      resolve(`${S3_ENDPOINT}${S3_BUCKET}/${path}/${name}`);
+      resolve(`${S3_ALIAS}/${path}/${name}`);
     } catch (err) {
       reject(err);
     }
