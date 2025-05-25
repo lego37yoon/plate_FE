fec9a84957e5fb627eefa3b7a77bb034cc1a7ee0
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-05-25 16:58:36 +0900

    fix: redirect loop on step 2 of signup

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,21 @@
 import type { LayoutServerLoad } from "./$types";
-import { error as kitError } from "@sveltejs/kit";
+import { error as kitError, redirect } from "@sveltejs/kit";
 import type { UserInfo } from "../types/account";
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_PROJECT_URL } from "$env/static/public";
+import { localizeHref } from "$lib/paraglide/runtime";
 
-export const load: LayoutServerLoad = async ({ cookies, locals: { safeGetSession } }) => {
+export const load: LayoutServerLoad = async ({ url, cookies, locals: { safeGetSession } }) => {
   
-  const { session } = await safeGetSession();
+  const { session, user } = await safeGetSession();
   const userInfo = cookies.get("user");
 
   let userData : UserInfo | null = null;
 
-  if (session && session.user) {
+  if (session && user) {
     if (userInfo) {
       userData = JSON.parse(userInfo);
-    } else {
-      const req = await fetch(`${PUBLIC_SUPABASE_PROJECT_URL}/rest/v1/user?uid=${session.user.id}`, {
+    } else if (!url.pathname.includes("/account/signup/step2")) {
+      const req = await fetch(`${PUBLIC_SUPABASE_PROJECT_URL}/rest/v1/user?uid=${user.id}`, {
         method: "GET",
         headers: {
           "apiKey": PUBLIC_SUPABASE_ANON_KEY,
@@ -30,9 +31,11 @@ export const load: LayoutServerLoad = async ({ cookies, locals: { safeGetSession
           if (userData) {
             cookies.set("user", userData.toString(), { path: "/" });
           }
+        } else {
+          throw redirect(303, localizeHref("/account/signup/step2"));
         }
       } else {
-        kitError(401, "userInfo.NotFound");
+        throw redirect(303, localizeHref("/account/signup/step2"));
       }
     }
   }
