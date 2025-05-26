b0f04d4cf39fb138c7766f482792c3ff136ef810
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-05-27 01:20:48 +0900

    feat: add project home with locale (partially works)

---
+++ src/routes/projects/[id]/+page.server.ts
@@ -0,0 +1,12 @@
+import type { PageServerLoad } from "./$types";
+import { error as kitError } from "@sveltejs/kit";
+
+export const load: PageServerLoad = async ({ locals: { supabase }}) => {
+  const { data, error } = await supabase.from("projects").select();
+
+  if (error) {
+    throw kitError(500, error.message); 
+  } else {
+    return { projects: data };
+  }
+}
