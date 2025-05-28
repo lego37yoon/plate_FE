e6938c18f990f86247bf582e63a490bf8643300f
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-05-28 18:04:18 +0900

    feat: define form data

--- src/types/others.ts
+++ src/types/others.ts
@@ -1,1 +1,16 @@
-type DocCategories = "none" | "others" | "style" | "common" | "community" | "brochure";
+export type DocCategories = "none" | "others" | "style" | "common" | "community" | "brochure";
+
+export type ProjectForm = {
+  name: string,
+  version: string,
+  desc: string,
+  manager: number,
+  resource: string[],
+  documents: {
+    path: string,
+    category: DocCategories,
+    rule: boolean
+  }[],
+  src_lang: string,
+  supported_lang: string[]
+};
