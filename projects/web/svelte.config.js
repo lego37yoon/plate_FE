a95f5c9356d8f33bc40799f11fc32648d293add4
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-08-26 01:34:03 +0900

    (web)init: remove deprecated mdsvex

--- projects/web/svelte.config.js
+++ projects/web/svelte.config.js
@@ -1,13 +1,10 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex({
-		layout: "./src/components/DocViewer.svelte"
-	})],
+	preprocess: [vitePreprocess()],
 	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte']
 };
 
 export default config;
