c90b51c614ed31fb2f5d824c8fb49a0b1a4b0ae1
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-08-26 02:21:31 +0900

    (web)feat: apply default style for inline documents

--- projects/web/src/components/DocViewer.svelte
+++ projects/web/src/components/DocViewer.svelte
@@ -17,7 +17,7 @@
     }
 </script>
 
-<section id="docs" class="shadow-md -ms-4 ps-6 py-4 pe-4 rounded-xl my-2 me-2 bg-white z-0 flex flex-col shrink "> 
+<section id="docs" class="shadow-md -ms-4 ps-6 py-4 pe-4 rounded-xl my-2 me-2 bg-white z-0 flex flex-col shrink"> 
     <div id="docs-buttonset" class="flex justify-between items-center">
         <h1 class="text-3xl text-primary">{doc.meta ? doc.meta.title.slice(0, -3) : "Untitled"}</h1>
         <Button.Root onclick={closeViewer} class="rounded-md p-2 bg-secondary">
@@ -41,7 +41,80 @@
         </p>
          -->
     </div>
-    <div id="docs-view">
+    <div id="docs-view" class="font-normal text-wrap min-w-0">
         {@html renderResult}
     </div>
 </section>
+
+<style>
+    #docs-view :global(h1),
+    #docs-view :global(h2),
+    #docs-view :global(h3),
+    #docs-view :global(h4),
+    #docs-view :global(h5),
+    #docs-view :global(h6),
+    #docs-view :global(ol),
+    #docs-view :global(ul),
+    #docs-view :global(pre) {
+        margin: 0.25rem 0;
+    }
+
+    #docs-view :global(h1) {
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
+
+    #docs-view :global(h2) {
+        font-size: 1.6rem;
+    }
+
+    #docs-view :global(h3) {
+        font-size: 1.4rem;
+        font-weight: 500;
+    }
+
+    #docs-view :global(h4) {
+        font-size: 1.2rem;
+    }
+
+    #docs-view :global(h5) {
+        font-size: 1.1rem;
+        font-weight: 500;
+    }
+
+    #docs-view :global(h6) {
+        font-size: 1.1rem;
+    }
+
+    #docs-view :global(p) {
+        text-indent: 0.25rem;
+    }
+
+    #docs-view :global(a) {
+        text-decoration: underline;
+        color: var(--color-primary);
+    }
+
+    #docs-view :global(a img) {
+        display: inline;
+    }
+
+    #docs-view :global(pre) {
+        text-wrap: wrap;
+        background: var(--color-gray-100);
+        padding: 1rem;
+        border-radius: 4px;
+    }
+
+    #docs-view :global(ol), #docs-view :global(ul) {
+        margin-inline-start: 2rem;
+    }
+
+    #docs-view :global(ol) {
+        list-style: decimal;
+    }
+
+    #docs-view :global(ul) {
+        list-style: disc;
+    }
+</style>
